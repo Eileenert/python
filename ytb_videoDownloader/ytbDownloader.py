b28d9eee18182f6a8ab5af66992efc35bfdaff8a
Eileen Rheinboldt-Tran <eileenr10.ert@gmail.com>
2020-12-17 19:01:03 +0100

    have the possibility of trying to download other subtitles if the first ones do not work

--- ytb_videoDownloader/ytbDownloader.py
+++ ytb_videoDownloader/ytbDownloader.py
@@ -61,39 +61,53 @@ while True:
                 # we download the subtitles
                 elif subtitle_dwl.lower() == "y":
                     while True:
-                        i = 0
-                        #list of subtitles with a number for each one
-                        for sub in subtitle:
-                            print(f"Press {i} for this subtitle: {sub}")
-                            i += 1
+                        while True:
+                            i = 0
+                            #list of subtitles with a number for each one
+                            for sub in subtitle:
+                                print(f"Press {i} for this subtitle: {sub}")
+                                i += 1
 
-                        #choose the subtitles
-                        language = input("Press the number: ")
-                        try:    #verify that it's a correct number
-                            language = int(language)
+                            #choose the subtitles
+                            language = input("Press the number: ")
+                            try:    #verify that it's a correct number
+                                language = int(language)
 
-                            if language > len(subtitle):
-                                print("please enter a correct number")
+                                if language > len(subtitle):
+                                    print("please enter a correct number")
+
+                                else:
+                                    break
+                            except:
+                                print("Please enter a number")
+
+                        try:
+                            # generate subtitle's text
+                            subtitle_text = subtitle[language].generate_srt_captions()
+                            with open(f"subtitle{language}.srt", "wt") as file:
+                                # write subtitle's text
+                                file.write(f"{subtitle_text}")
+
+                            print("Download completed!")
+                            # quit
+                            break
+
+                        except:
+                            print("Impossible to download this subtitle")
+                            os.remove(f"subtitle{language}.srt")
+
+                            retry=input("try other subtitles? y/n: ")
+
+                            if retry.lower()=="y":
+                                pass
+
+                            elif retry.lower()=="n":
+                                break
 
                             else:
-                                break
-                        except:
-                            print("Please enter a number")
+                                print("Please enter 'y' or 'n'")
 
-                    try:
-                        # generate subtitle's text
-                        subtitle = subtitle[language].generate_srt_captions()
-                        with open(f"subtitle{language}.srt", "wt") as file:
-                            # write subtitle's text
-                            file.write(f"{subtitle}")
-
-                        print("Download completed!")
-                        # quit
-                        break
-
-                    except:
-                        print("Impossible to download this subtitle")
-                        break
+                    break
 
                 else:
                     print("Please enter 'y' or 'n'")
