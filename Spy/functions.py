526f1ecd54100769c94435cfe6f3eac31c99808d
Eileen Rheinboldt-Tran <eileenr10.ert@gmail.com>
2020-12-27 13:56:03 +0100

    correct and write my code a little more correctly using better tkinter

--- Spy/functions.py
+++ Spy/functions.py
@@ -6,61 +6,14 @@ place = ["Parking", "Home", "Office", "Airport", "Bakery", "Bank", "Bar", "Books
          "Hotel", "Gallery", "Jail", "Library", "Mall", "Museum", "Pharmacy", "Police station", "Post office", "Park", "Restaurant", "Supermarket", "Zoo"]
 
 
-def see_rules():
-
-    while True:
-        rules = input("Do you want to see the rules? (y/n): ")
-
-        if rules.lower() == "y":
-            # rules
-            print("""\nEach in turn you are going to receive your role (one person at a time must watch his role).
+rules = """\nEach in turn you are going to receive your role (one person at a time must watch his role).
 The villagers receive at the same time the place, which is the same for each one. 
 There are 2 spies among you, you must unmask them. 
 The spies do not receive the location and must convince the villagers that they know the location to not be unmask.
-When you are ready, you must each vote 2 people you think are the spies\n""")
-
-            break
-        elif rules.lower() == "n":
-            break
-
-        else:  # if the user didn't choose y or n
-            print("Please enter y or n")
+When you are ready, you must each vote 2 people you think are the spies\n"""
 
 
-# choose the number of players
-def numberPlayers():
-    while True:
-        nbr_players = input("Please enter the number of players: ")
-        try:
-            nbr_players = int(nbr_players)  # convert str into int
-
-            if nbr_players <= 3:  # verify the number of players
-                print("You must be at least 4 to play")
-                raise ValueError
-
-        except ValueError:
-            print("Please enter a correct number")
-
-        else:
-            break
-
-    return nbr_players
-
-# choose a lace randomly
-
-
+# choose a place randomly
 def random_place():
     thePlace = choice(place)
     return thePlace
-
-# spy person
-
-
-def spy():
-    return f"\nYou are a spy \n"
-
-# villager person
-
-
-def villager(thePlace):
-    return f"{thePlace} \nYou are a villager \n"
--- Spy/game.py
+++ Spy/game.py
@@ -1,66 +1,145 @@
 import functions as f
-from random import shuffle
+import random
 import tkinter as tk
 
 
-def game():
-    # decide to see the rules or not
+class Interface(tk.Frame):
 
-    f.see_rules()
+    """main window"""
 
-    # to know how many players there are
-    numberPlayers = f.numberPlayers()
+    def __init__(self, window, **kwargs):
+        tk.Frame.__init__(self, window, width=750, height=575, **kwargs)
+        self.pack(fill=tk.BOTH)
 
-    # choose a random place
-    thePlace = f.random_place()
+        self.place = f.random_place()  # the place
 
-    # number of villager and spies in the party
-    villagers = numberPlayers - 2
-    spies = 2
+        self.players_number = 0  # number of players
+        self.spies = 0  # number of spies
+        self.villager = 0  # number of villager
+        self.spies_or_villager = ""  # if the player is a spy or a villager
 
-    # add the villagers
-    vi = []
-    v = 0
-    while v < villagers:
-        vi.append(f.villager(thePlace))
-        v += 1
+        # ask if we want to see the rules
+        self.message = tk.Label(self, text="Do you want to see the rules")
+        self.message.pack()
 
-    # add the spies
-    sp = []
-    s = 0
-    while s < spies:
-        sp.append(f.spy())
-        s += 1
+        self.no_button = tk.Button(  # no button
+            self, text="No", fg="red", command=self.nbr_player)
+        self.no_button.pack(side="left")
 
-    # put together villagers and spies
-    visp = sp + vi
-    shuffle(visp)  # mix the order
+        self.yes_button = tk.Button(  # yes button
+            self, text="Yes", fg="green", command=self.display_rules)
+        self.yes_button.pack(side="right")
 
-    # display the roles
-    i = 0
-    while i < numberPlayers:
-        root = tk.Tk()
-        root.title("Role")
-        a = visp.pop()
-        input("\nPress enter or anything to see your card: \n")
-        tk.Label(root, text=a).pack()
-        tk.Button(root, text="ok", command=root.destroy).pack()
-        root.mainloop()
+        self.next_button = tk.Button(  # next button
+            self, text="Next", fg="red", command=self.nbr_player)
 
-        i += 1
+        self.var_text_entry = tk.StringVar()  # variable with entry_line text
+        self.entry_line = tk.Entry(
+            self, textvariable=self.var_text_entry)  # entry line
 
-    def restart():
-        rst = input("Restart Game? (y/n): ")
-        if rst.lower() == "y":
-            game()
-        elif rst.lower() == "n":
-            pass
-        else:  # if the user didn't choose y or n
-            print("Please enter y or n")
-            restart()
+        # enter button
+        self.enter_button = tk.Button(
+            self, text="Enter", fg="red", command=self.verify_nbr_player)
 
-    restart()
+        # next player button
+        self.next_player_button = tk.Button(
+            self, text="Next Player", fg="blue", command=self.next_player)
+        self.text_or_white = "text"
+
+        self.number_of_card = 0
+
+    def display_rules(self):
+        """If we click on the Yes button to see the rules"""
+
+        self.message["text"] = f.rules  # modify the text
+        self.no_button.pack_forget()  # remove button no
+        self.yes_button.pack_forget()  # remove button yes
+
+        self.next_button.pack()  # add the next button
+
+    def nbr_player(self):
+        """ If next_button or no_button is clicked
+
+        We enter the number of player 
+        """
+
+        self.next_button.pack_forget()  # remove button next
+        self.yes_button.pack_forget()  # remove button yes
+        self.no_button.pack_forget()  # remove button no
+
+        # modify the text
+        self.message["text"] = "Please enter the number of players"
+        self.entry_line.pack()  # add an entry line
+        self.enter_button.pack(side="bottom")  # add a enter button
+
+    def verify_nbr_player(self):
+        """We verify that the number of player is correct"""
+        try:
+            self.players_number = int(self.var_text_entry.get())
+
+            if self.players_number <= 3:  # verify the number of players
+                raise ValueError
+
+        except ValueError:
+            self.message["text"] = "You must be at least 4 to play \nPlease enter a correct number"
+
+        else:
+            self.game()
+
+    def game(self):
+        self.entry_line.pack_forget()   # remove entry_line
+        self.enter_button.pack_forget()  # remove enter button
+
+        self.message["text"] = ""
+        self.next_player_button.pack()
+
+    def next_player(self):
+
+        if self.number_of_card == self.players_number:
+            self.destroy()
+
+        # choose if the player is a spy or a villager
+        if self.spies != 2 and self.villager != self.players_number-2:
+            self.spies_or_villager = random.choice(["villager", "spy"])
+
+        # there can only be 2 spies
+        elif self.spies == 2:
+            self.spies_or_villager = "villager"
+
+        # the number of villagers is limited to the number of players minus the two spies
+        elif self.villager == self.players_number-2:
+            self.spies_or_villager = "spy"
+
+        # choose between a white card or role
+        if self.text_or_white == "white":
+            self.message["text"] = ""
+            self.text_or_white = "text"
+
+        elif self.text_or_white == "text":
+
+            # if the player is a villager we display his card and place
+            if self.spies_or_villager == "villager":
+                self.message["text"] = f"{self.place} \nYou are a villager \n"
+                self.villager += 1
+
+            # if the player is a spy we display his card
+            elif self.spies_or_villager == "spy":
+                self.message["text"] = f"\nYou are a spy \n"
+                self.spies += 1
+
+            self.text_or_white = "white"
+
+            self.number_of_card += 1
 
 
 if __name__ == '__main__':
-    game()
+    # we create our interface
+    window = tk.Tk()
+
+    window.geometry('700x300')
+    window.resizable(0, 0)
+
+    interface = Interface(window)
+
+    interface.mainloop()
+    interface.destroy()
